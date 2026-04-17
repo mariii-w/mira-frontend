3c7c874e55b4b859c692b9d2a04fce02646af8a2
mariii-w <voloderm@gmail.com>
2026-04-17 12:59:55 +0200

    changed the testing framework from jest to vitest

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-};
--- src/__tests__/example.test.ts
+++ src/__tests__/example.test.ts
@@ -1,3 +1,5 @@
+import { describe, it, expect } from 'vitest'
+
 describe('Sanity check', () => {
   it('passes', () => {
     expect(1 + 1).toBe(2);
