a113745500ccd1716f0c58d3b99e48336d1e0c9b
mariii <mvoloder@hof-university.de>
2026-05-08 15:51:20 +0200

    refactor(a11y): drop AccessibilityProvider, fully migrated to Zustand for accessibility preference state

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import { AccessibilityProvider } from './providers/AccessibilityProvider'
 import './globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AccessibilityProvider>
-      <RouterProvider router={router} />
-    </AccessibilityProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
