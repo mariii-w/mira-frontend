1fd0f92d18e56ca2d33dd07c15604fbebc8a4880
mariii <mvoloder@hof-university.de>
2026-05-10 11:00:58 +0200

    ci: add test and coverage reports to pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,15 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), tailwindcss(), react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['@testing-library/jest-dom/vitest'],
-    globals: true,
+  environment: 'jsdom',
+  setupFiles: ['@testing-library/jest-dom/vitest'],
+  globals: true,
+  reporters: ['default', 'junit'],
+  outputFile: './junit.xml',
+  coverage: {
+    provider: 'v8',
+    reporter: ['text', 'cobertura', 'html'],
+    reportsDirectory: './coverage',
   },
+},
 })
