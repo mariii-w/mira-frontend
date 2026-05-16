4576e712d33bf06bbb8b8b203ce4f5cbb630d9ba
mariii <mvoloder@hof-university.de>
2026-05-16 14:15:51 +0200

    feat(auth): wire silent login boot exchange to root route on app mount

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,18 @@
+
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { exchangeRefreshForAccess } from '../stores/auth'
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+function RootComponent() {
+  useEffect(() => {
+    exchangeRefreshForAccess()
+  }, [])
+
+  return <Outlet />
+}
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: RootComponent,
 })
