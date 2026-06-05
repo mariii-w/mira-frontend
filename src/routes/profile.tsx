3c7232170bd4c74eed62a4ee12cde85fbaeb1ee1
Blue3Andy <blueandydev@gmail.com>
2026-06-05 15:01:13 +0200

    create cusotme edit serice card

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,13 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Search, Check, ArrowRight, ChevronRight, ChevronLeft, ClipboardPen, MapPin  } from 'lucide-react'
+import { Check, ClipboardPen, MapPin, Briefcase, Plus, Calendar, Mail, History} from 'lucide-react'
 import { useState, useRef } from 'react'
 import { Navbar } from '../components/Navbar'
 import { Button } from '../components/Button'
-import { CategoryCard } from '../components/CategoryCard'
-import { ProviderCard } from '../components/ProviderCard'
-import { Logo } from '../components/Logo'
 import { AvatarIcon } from '../components/AvatarIcon'
-import { ServiceCard, type ServiceCardProps } from '../components/ServiceCard'
+import { ServiceCardEdit, type ServiceCardEditProps } from '../components/ServiceCardEdit'
 
 /* eslint-disable react-refresh/only-export-components */
 export const Route = createFileRoute('/profile')({ component: Profile })
@@ -22,45 +19,27 @@ function Profile() {
 
   const userDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-  const serviceListings: ServiceCardProps[] = [
+  const serviceListings: ServiceCardEditProps[] = [
     {
       link: '/service/1',
       pictureLink: 'https://images.ctfassets.net/5i1m3im8l2b5/5yLgQr5c29UlkwTN7nDyUY/6f5d6a7d6b8e14129d75c72ecb1413a7/What_is_tech_support.jpg?w=1200&h=630&fl=progressive&q=50&fm=jpg',
-      location: 'Berlin, Germany',
-      providerFirstName: 'John',
-      providerLastName: 'Doe',
-      varified: true,
       label: 'Web Development',
       description: 'Professional web development services',
-      badges: [{ text: 'React', variant: 'primary' }, { text: 'TypeScript', variant: 'primary' }],
-      hourRate: 50,
-      distance: 0
+      status: 'active'
     },
     {
       link: '/service/2',
       pictureLink: 'https://images.ctfassets.net/5i1m3im8l2b5/5yLgQr5c29UlkwTN7nDyUY/6f5d6a7d6b8e14129d75c72ecb1413a7/What_is_tech_support.jpg?w=1200&h=630&fl=progressive&q=50&fm=jpg',
-      location: 'Berlin, Germany',
-      providerFirstName: 'John',
-      providerLastName: 'Doe',
-      varified: true,
       label: 'UI Design',
-      description: 'Modern UI/UX design consultation',
-      badges: [{ text: 'Figma', variant: 'primary' }, { text: 'Design', variant: 'primary' }],
-      hourRate: 45,
-      distance: 0
+      description: 'Modern UI/UX design consultation lorem',
+      status: 'active'
     },
     {
-      link: '/service/2',
+      link: '/service/3',
       pictureLink: 'https://images.ctfassets.net/5i1m3im8l2b5/5yLgQr5c29UlkwTN7nDyUY/6f5d6a7d6b8e14129d75c72ecb1413a7/What_is_tech_support.jpg?w=1200&h=630&fl=progressive&q=50&fm=jpg',
-      location: 'Berlin, Germany',
-      providerFirstName: 'John',
-      providerLastName: 'Doe',
-      varified: true,
       label: 'UI Design',
       description: 'Modern UI/UX design consultation',
-      badges: [{ text: 'Figma', variant: 'primary' }, { text: 'Design', variant: 'primary' }],
-      hourRate: 45,
-      distance: 0
+      status: 'draft'
     }
   ]
 
@@ -73,7 +52,6 @@ function Profile() {
         </section>
         <section>
             <div className="container mx-auto -mt-20 p-4 grid grid-cols-3 gap-5 items-start w-286">
-
                 {/* User Info - row 1, left 2/3 */}
                 <div className="col-span-2">
                     <div className='flex flex-row items-center gap-4'>
@@ -84,7 +62,7 @@ function Profile() {
                             <div className='flex flex-row gap-4 items-center'>
                                 <h1 className="text-3xl font-semibold">John Doe</h1>
                                 {isVerified? <p className="text-sm font-bold text-primary flex items-center gap-1"> <Check /> Verifiziert</p> : <p className="text-sm font-bold"></p>}
-                                <Button size="md" trailingIcon={<ClipboardPen />}>Edit Profile</Button>
+                                <Button size="md" trailingIcon={<ClipboardPen />}>Bearbeiten</Button>
                             </div>
                             <div>
                                 {isProvider? <p className="text-xl font-bold text-primary">Dienstleiter</p> : <p className="text-sm text-accent">Kunde</p>}
@@ -98,21 +76,29 @@ function Profile() {
 
                 {/* Access Buttons - row 1-3, right 1/3 */}
                 <div className="col-start-3 row-start-2 row-span-3 bg-linen border border-border rounded-2xl p-6">
-                    <div className="flex flex-col mx-auto">
-                        <Button variant="primary" size="lg" onClick={() => setActiveTab('account')}>
-                            Meine Anzeigen
+                    <p className="text-h1 font-bold">Menü</p>
+                    <div className="w-full mx-auto h-px bg-border m-5" />
+                    <div className="flex flex-col mx-auto w-70">
+                        <Button variant="primary" size="lg" leadingIcon={<Briefcase />} onClick={() => setActiveTab('account')}>
+                           <p className='text-lg font-bold'>Meine Anzeigen</p>
                         </Button>
-                        <Button variant="accent" size="lg" onClick={() => setActiveTab('listings')} className="mt-4">
-                            Anzeige erstellen
+                        <Button variant="accent" size="lg" leadingIcon={<Plus />} onClick={() => setActiveTab('listings')} className="mt-4">
+                            <p className='text-lg font-bold'>Anzeige erstellen</p>
                         </Button>
-                        <Button variant="secondary" size="lg" onClick={() => setActiveTab('listings')} className="mt-4">
-                            Kalender
+                        <Button variant="secondary" size="lg" leadingIcon={<Calendar />} onClick={() => setActiveTab('listings')} className="mt-4">
+                            <p className='text-lg font-bold'>Kalender</p>
                         </Button>
-                        <Button variant="secondary" size="lg" onClick={() => setActiveTab('listings')} className="mt-4">
-                            Meine Buchungen
+                        <Button variant="secondary" size="lg" leadingIcon={<Mail />} onClick={() => setActiveTab('listings')} className="mt-4">
+                            <p className='text-lg font-bold'>Meine Buchungen</p>
                         </Button>
-                        <Button variant="secondary" size="lg" onClick={() => setActiveTab('listings')} className="mt-4">
-                            Verlauf
+                        <Button variant="secondary" size="lg" leadingIcon={<History />} onClick={() => setActiveTab('listings')} className="mt-4">
+                            <p className='text-lg font-bold'>Verlauf</p>
+                        </Button>
+                    </div>
+                    <div className="w-full mx-auto h-px bg-border m-5" />
+                    <div className="flex flex-col mx-auto w-60">
+                        <Button variant="primary" size="lg" leadingIcon={<Check />} onClick={() => setActiveTab('account')}>
+                           <p className='text-lg font-bold'>Verifizieren</p>
                         </Button>
                     </div>
                 </div>
@@ -130,7 +116,7 @@ function Profile() {
                     <h2 className="mb-4">Meine Anzeigen</h2>
                     <div className="flex flex-col gap-4">
                         {serviceListings.map((listing) => (
-                            <ServiceCard key={listing.link} {...listing} />
+                            <ServiceCardEdit key={listing.link} {...listing} />
                         ))}
                     </div>
                 </div>
