9461806c72c53dec1b58563a7fbb24c796236b56
frederik <termite_pilgrim04@icloud.com>
2026-06-14 16:42:17 +0200

    fix(openapi): add lock file & yml

--- orval.config.ts
+++ orval.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
     input: {
       target: "./mira-api.yaml",
     },
-    hooks: {
-      afterAllFilesWrite: "node ./scripts/mark-generated-openapi.mjs",
-    },
     output: {
       target: "./src/api/mira.ts",
       schemas: "./src/api/model",
