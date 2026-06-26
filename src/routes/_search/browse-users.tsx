8e4f7256079c422cd131492345056c121b2070f2
frederik <termite_pilgrim04@icloud.com>
2026-06-26 13:26:44 +0200

    fix(head): browse users head

--- src/routes/_search/browse-users.tsx
+++ src/routes/_search/browse-users.tsx
@@ -1,8 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SearchUsersPage } from '../../components/search/pages/SearchUsersPage.tsx'
-import { browseUsersSearchSchema } from '../../components/search/searchSchemas'
+import { createFileRoute } from "@tanstack/react-router";
+import { SearchUsersPage } from "../../components/search/pages/SearchUsersPage.tsx";
+import { browseUsersSearchSchema } from "../../components/search/searchSchemas";
+import { createPageMeta } from "../../lib/headers";
 
-export const Route = createFileRoute('/_search/browse-users')({
+export const Route = createFileRoute("/_search/browse-users")({
+  head: () =>
+    createPageMeta({
+      title: "Browse Users",
+      description:
+        "Browse Mira users and providers by profile type, keyword and location.",
+      path: "/browse-users",
+    }),
   validateSearch: browseUsersSearchSchema,
   component: SearchUsersPage,
-})
+});
